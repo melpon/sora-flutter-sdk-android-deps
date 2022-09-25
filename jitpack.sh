a18825f00f5910f66ebd5efe4ac3a6c87e5270ec
melpon <shigemasa7watanabe+github@gmail.com>
2022-09-25 22:27:39 +0900

    修正

--- jitpack.sh
+++ jitpack.sh
@@ -30,7 +30,7 @@ pushd $INSTALL_DIR
 popd
 
 mvn install:install-file \
-  -Dfile=_source/$SOURCE_DIR/$SORA_FILENAME \
+  -Dfile=$SOURCE_DIR/$SORA_FILENAME \
   -Dversion=${SORA_VERSION} \
   -Dpackaging=zip
   -DgroupId=com.github.melpon \
